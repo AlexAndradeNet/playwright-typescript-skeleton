504c71d54a8be02f369685cddbf9a5475a8f32f8
Alex Andrade <alexander.andrade@gmail.com>
2024-09-01 16:23:11 -0500

    🔧 standardize reporting paths

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,12 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI != null ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: [
+        [
+            'html',
+            { outputFolder: './.run/reports/report-html', open: 'always' },
+        ],
+    ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
