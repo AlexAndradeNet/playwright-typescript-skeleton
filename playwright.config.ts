6c287cf2daafa1e34d452f4552a906c6958c7895
Alex Andrade <alexander.andrade@gmail.com>
2024-09-01 16:22:48 -0500

    🚨 fixing linting warnings

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,15 +1,19 @@
-/* eslint-disable */
-
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+/**
+ * Convert the module URL to a file path and extract the directory.
+ */
+const filename: string = fileURLToPath(import.meta.url);
+const dirname: string = path.dirname(filename);
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-import path from 'node:path';
-
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -19,13 +23,12 @@ export default defineConfig({
     /* Run tests in files in parallel */
     fullyParallel: true,
     /* Fail the build on CI if you accidentally left test.only in the source code. */
-    forbidOnly: !!process.env.CI,
+    forbidOnly: !(process.env.CI == null),
     /* Retry on CI only */
-    retries: process.env.CI ? 2 : 0,
+    retries: process.env.CI != null ? 2 : 0,
     /* Opt out of parallel tests on CI. */
-    workers: process.env.CI ? 1 : undefined,
+    workers: process.env.CI != null ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
