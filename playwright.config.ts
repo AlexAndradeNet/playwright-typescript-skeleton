9e90ac2e9cfca4dd5dfc390350043e4d2768f614
Alex Andrade <alexander.andrade@gmail.com>
2024-09-01 16:24:14 -0500

    💚 implement GitHub actions fast cache and store reports as artifacts

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,10 +30,7 @@ export default defineConfig({
     workers: process.env.CI != null ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
-        [
-            'html',
-            { outputFolder: './.run/reports/report-html', open: 'always' },
-        ],
+        ['html', { outputFolder: './.run/reports/report-html', open: 'never' }],
     ],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
